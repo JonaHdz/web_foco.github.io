fedc5c4f3d5469d3c9040d61cf0031e9602b6936
Jonathan Hernández Martínez <126419190+JonaHdz@users.noreply.github.com>
2023-10-22 13:39:05 -0600

    Update foco.js

--- foco.js
+++ foco.js
@@ -1,40 +1,29 @@
-// se ejecuta al cargar la pagina completa
-window.addEventListener('load', function () {
-    // llama a la funcion FontFaceSetLoadEvent
-    // observe como puede esar la declaracion abajo de la llamada
+window.addEventListener('load', function(){
     foco();
 });
 
-function foco() {
-    //le agrega una clase al lobby
+function foco(){
     document.querySelector('body').classList.add('oscuridad');
-
-    //agregar el titulo
-    const titulo = document.createElement("h1");
-    titulo.classList.add("titulo");
+    const titulo = document.createElement('h1');
+    titulo.classList.add('titulo');
     const anio = new Date().getFullYear();
-    titulo.innerText = `Desarrollo De Sistema Web ${anio}`;
+    titulo.innerText = `Desarrollo de sistemas weba: ${anio}`
     document.body.appendChild(titulo);
+    
+    const img = document.createElement('img');
+    img.classList.add('foco');
+    img.setAttribute('src', '/foco-apagado.jpg')
 
-    // imagen
-    const image = document.createElement("img"); //crea un elemento html imagen
-    image.classList.add('foco');//lea gregar una clase
-    image.setAttribute('src', '/foco-apagado.jpg');
-
-    // agregamos una funcion en el evento clic
-    image.addEventListener('clic', () => {
-        // busca si la cadena apagado esta en el atributo src
-        if (image.setAttribute('src', '/foco-prendido.jpg')) {
+    img.addEventListener('click', () =>{
+        if(img.src.indexOf('apagado') > 0){
+            img.setAttribute('src', '/foco-prendido.jpg');
             document.body.classList.remove('oscuridad');
             document.body.classList.add('luz');
-        } else {
-            // cambiamos la imagen y el fonod
-            image.setAttribute('src', '/foco-apagado.jpg');
+        } else{
+            img.setAttribute('src', '/foco-apagado.jpg');
             document.body.classList.remove('luz');
             document.body.classList.add('oscuridad');
         }
-    })
-    //agrega la imagen al DOM
-    
+    });
+    document.body.appendChild(img);
 }
-document.body.appendChild(img)
